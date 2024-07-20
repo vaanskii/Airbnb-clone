37cc61e36becdc44e0dda5a3967775a58aebbd87
vaanskii <giorgivanadze03@gmail.com>
2024-07-20 18:19:56 +0400

    Integrate ListingItemView - Hardoced view where is a rectangle and details for  room

--- Airbnb clone/Core/Listings/ListingItemView.swift
+++ Airbnb clone/Core/Listings/ListingItemView.swift
@@ -11,9 +11,42 @@ struct ListingItemView: View {
     var body: some View {
         VStack(spacing: 8) {
             // Images
+            Rectangle()
+                .frame(height: 320)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
             
             // Listing details
+            HStack(alignment: .top) {
+                //details
+                VStack(alignment: .leading) {
+                    Text("Miami, Florida")
+                        .fontWeight(.semibold)
+                    
+                    Text("12 mi away")
+                        .foregroundStyle(.gray)
+                    
+                    Text("Nov 3 - 10")
+                        .foregroundStyle(.gray)
+                    
+                    HStack(spacing: 4) {
+                        Text("$567")
+                            .fontWeight(.semibold)
+                        Text("night")
+                    }
+                }
+                
+                Spacer()
+                
+                //rating
+                HStack(spacing: 2) {
+                    Image(systemName: "star.fill")
+                    
+                    Text("4.86")
+                }
+            }
+            .font(.footnote)
         }
+        .padding()
     }
 }
 
