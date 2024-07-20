82b5910916760b2b6df42c9054998f0bbadf22c6
vaanskii <giorgivanadze03@gmail.com>
2024-07-20 21:03:32 +0400

    Integrate hardcoded ListingDetail view - Integrate component for litsting item carousel - Integrate hardcoded listingdetail view where user can see detailed information about hotel or room

---
+++ Airbnb clone/Core/Components/ListingImageCarouselView.swift
@@ -0,0 +1,18 @@
+//
+//  ListingImageCarouselView.swift
+//  Airbnb clone
+//
+//  Created by Giorgi Vanadze on 20.07.24.
+//
+
+import SwiftUI
+
+struct ListingImageCarouselView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListingImageCarouselView()
+}
---
+++ Airbnb clone/Core/Listings/ListingDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  ListingDetailView.swift
+//  Airbnb clone
+//
+//  Created by Giorgi Vanadze on 20.07.24.
+//
+
+import SwiftUI
+
+struct ListingDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListingDetailView()
+}
