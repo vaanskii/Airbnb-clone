b047dd0ec477b4983ef2666767980c09d0dce2ff
vaanskii <giorgivanadze03@gmail.com>
2024-07-20 18:54:33 +0400

    Integrate tabview in listingitem view to scroll images horizontally. - Created Search bar and integrate it in ExploreView

---
+++ Airbnb clone/Core/Explore/SearchAndFilterBar.swift
@@ -0,0 +1,18 @@
+//
+//  SearchAndFilterBar.swift
+//  Airbnb clone
+//
+//  Created by Giorgi Vanadze on 20.07.24.
+//
+
+import SwiftUI
+
+struct SearchAndFilterBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchAndFilterBar()
+}
